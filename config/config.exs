f42d39b0eeaf6b0ad334a726e17371bf68e7cf71
Kuret <r.littel@outlook.com>
2019-07-16 12:47:16 +0200

    add config to enable or disable server

--- config/config.exs
+++ config/config.exs
@@ -1,5 +1,3 @@
 use Mix.Config
 
-config :oauth_mock_server, port: 54_345
-
 import_config "#{Mix.env()}.exs"
--- lib/oauth_mock_server.ex
+++ lib/oauth_mock_server.ex
@@ -3,18 +3,24 @@ defmodule OauthMockServer do
   Documentation for OauthMockServer.
   """
 
-  use Application
+  def start(_type, _args), do: Supervisor.start_link([], strategy: :one_for_one)
 
-  def start(_type, _args) do
-    children = [
+  def start_link(options) do
+    [
       Plug.Cowboy.child_spec(
         scheme: :http,
         plug: OauthMockServer.Router,
-        options: [port: Application.get_env(:oauth_mock_server, :port) || 54_345]
+        options: [port: Keyword.get(options, :port, 54_345)]
       )
     ]
+    |> Supervisor.start_link(strategy: :one_for_one, name: OauthMockServer.Supervisor)
+  end
 
-    opts = [strategy: :one_for_one, name: OauthMockServer.Supervisor]
-    Supervisor.start_link(children, opts)
+  def child_spec(opts) do
+    %{
+      id: __MODULE__,
+      start: {__MODULE__, :start_link, [opts]},
+      type: :supervisor
+    }
   end
 end
