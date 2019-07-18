49e817111a6cf5849dfdb64c09e6f2c007119df0
Kuret <r.littel@outlook.com>
2019-07-18 13:47:27 +0200

    Update request logger to show params conditionally

--- lib/oauth_mock_server/plugs/request_logger.ex
+++ lib/oauth_mock_server/plugs/request_logger.ex
@@ -14,6 +14,9 @@ defmodule OauthMockServer.RequestLogger do
 
     Plug.Conn.register_before_send(conn, fn conn ->
       Logger.log(:info, fn ->
+        params = inspect(conn.params)
+        params_string = (params != "%{}" && "\nParams: #{params}") || ""
+
         redirect = Plug.Conn.get_resp_header(conn, "location")
         redirect_string = (redirect != [] && "\nRedirected: #{redirect}") || ""
 
@@ -21,10 +24,8 @@ defmodule OauthMockServer.RequestLogger do
         time_us = System.convert_time_unit(stop_time - start_time, :native, :microsecond)
         time_ms = div(time_us, 100) / 10
 
-        """
-        #{conn.status} #{conn.method} #{conn.request_path} (#{time_ms}ms)
-        Params: #{inspect(conn.params)}#{redirect_string}
-        """
+        "#{conn.status} #{conn.method} #{conn.request_path} (#{time_ms}ms)" <>
+          "#{params_string}#{redirect_string}"
       end)
 
       conn
