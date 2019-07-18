213904bc8e3fcf8bce59d8e347626958176a8212
Kuret <r.littel@outlook.com>
2019-07-18 12:07:17 +0200

    Add mix task to start the server

---
+++ lib/mix/tasks/server.ex
@@ -0,0 +1,27 @@
+defmodule Mix.Tasks.Server do
+  @shortdoc "Starts the mock server"
+  @moduledoc false
+
+  use Mix.Task
+
+  @doc false
+  def run(args) do
+    port = Application.get_env(:oauth_mock_server, :port, 54_345)
+
+    {:ok, _} = Application.ensure_all_started(:logger)
+    {:ok, _} = Application.ensure_all_started(:cowboy)
+
+    OauthMockServer.start_link(port: port)
+    Mix.shell().info("OauthMockServer started at port #{port}")
+
+    Mix.Tasks.Run.run(run_args() ++ args)
+  end
+
+  defp run_args do
+    if iex_running?(), do: [], else: ["--no-halt"]
+  end
+
+  defp iex_running? do
+    Code.ensure_loaded?(IEx) and IEx.started?()
+  end
+end
