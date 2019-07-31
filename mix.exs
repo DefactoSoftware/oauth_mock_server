6651e28e3f75ce5b99a1e0c979c5b32affac5908
Kuret <rick@littel.me>
2019-07-31 13:26:29 +0200

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule OauthMockServer.MixProject do
   def project do
     [
       app: :oauth_mock_server,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.8.0",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
